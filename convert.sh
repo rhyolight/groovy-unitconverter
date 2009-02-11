1d6cf17d248904de0591211da357030e04b3f7c2
Matthew Taylor <rhyolight@gmail.com>
2009-02-11 13:45:53 -0600

    Updated convert shell script with scripts for gradle and IDEA builds.

--- convert.sh
+++ convert.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-groovy -cp build/production/unitConverter src/main/org/groovy/example/Runner.groovy $@
+# This line works with the gradle build
+# groovy -cp build/classes src/main/groovy/org/groovy/example/Runner.groovy $@
+
+# This line works for IntelliJ's build
+groovy -cp out/production/unitConverter src/main/groovy/org/groovy/example/Runner.groovy $@
